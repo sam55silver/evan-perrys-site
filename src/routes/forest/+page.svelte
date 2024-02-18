c2bf6d23af9b0b5c1a3b762a8e93d2b7e091aa28
Sam Silver <sam55silver@gmail.com>
2024-02-18 11:12:22 -0400

    init astro

--- src/routes/forest/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import Forest from '$lib/assets/images/forest.jpg'
-	import ImageCard from '$lib/components/imageCard.svelte'
-</script>
-
-<ImageCard byWidth={false} imgSrc={Forest} title="Forest" />
--- src/routes/lighthouse/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import Lighthouse from '$lib/assets/images/lighthouse.jpg'
-	import ImageCard from '$lib/components/imageCard.svelte'
-</script>
-
-<ImageCard byWidth={true} imgSrc={Lighthouse} title="Lighthouse"/>
-
